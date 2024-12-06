31d9da01d9324b7617c24384a175141b168172b4
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-05 21:24:50 -0500

    Added: getPriceAttribute method and fix syncPrices

--- Traits/HasPrices.php
+++ Traits/HasPrices.php
@@ -8,6 +8,7 @@ class HasPrices
   {
     return [
       'relations' => ['prices'],
+      'methods' => ['getPriceAttribute', 'getDefaultZone'],
       'events' => [
         'createdWithBindings' => 'syncPrices',
         'updatedWithBindings' => 'syncPrices',
@@ -23,23 +24,38 @@ class HasPrices
    */
   public function syncPrices($params)
   {
+    //Get the model
+    $model = $params['model'];
     $prices = $params['bindings']['data']['prices'] ?? null;
+    $price = $params['bindings']['data']['price'] ?? null;
+    $priceRepository = app("Modules\Iprice\Repositories\PriceRepository");
+
     //Validate if model need to sync prices
     if ($prices) {
       //Init the needed repositories
-      $priceRepository = app("Modules\Iprice\Repositories\PriceRepository");
       $tariffRepository = app("Modules\Iprice\Repositories\TariffRepository");
       $tariffableRepository = app("Modules\Iprice\Repositories\TariffableRepository");
-      //Get the model
-      $model = $params['model'];
       //Sync prices
-      foreach ($prices as $zone => $price) {
-        //update or create the price
-        $iprice = $priceRepository->updateOrCreate([
-          'entity_type' => get_class($model),
-          'entity_id' => $model->id,
-          'zone' => $zone
-        ], ['price' => $price['value']]);
+      foreach ($prices as $price) {
+        $zoneId = $price['zone_id'] ?? null;
+        $zoneSystemName = $price['zone_system_name'] ?? null;
+
+        if(!isset($zoneId) && isset($zoneSystemName)) {
+          $zoneRepository = app('Modules\Iprice\Repositories\ZoneRepository');
+          $zone = $zoneRepository->getItem($zoneSystemName, json_decode(json_encode(['filter' => ['field' => 'system_name']])));
+          $zoneId = $zone->id ?? null;
+        }
+        $iprice = null;
+        if($zoneId) {
+          //update or create the price
+          $iprice = $priceRepository->updateOrCreate([
+            'entity_type' => get_class($model),
+            'entity_id' => $model->id,
+            'zone_id' => $zoneId
+          ], ['value' => $price['value']]);
+        }
+
+        if(!isset($iprice)) return;
         //Sync the price tariffs
         foreach (($price['tariffs'] ?? []) as $tariff) {
           if (isset($tariff['id']) || isset($tariff['system_name'])) {
@@ -64,6 +80,19 @@ class HasPrices
           }
         }
       }
+    } else if(isset($price)) {
+      $defaultZone = $model->getDefaultZone();
+      $zoneId = $defaultZone->id ?? null;
+
+      //Ignore if not exist default zone
+      if($zoneId) {
+        //update or create the price
+        $priceRepository->updateOrCreate([
+          'entity_type' => get_class($model),
+          'entity_id' => $model->id,
+          'zone_id' => $zoneId
+        ], ['value' => $price]);
+      }
     }
   }
 
@@ -77,5 +106,27 @@ class HasPrices
   public function prices($model)
   {
     return $model->morphMany('Modules\Iprice\Entities\Price', 'entity');
+  }
+
+  public function getDefaultZone()
+  {
+    $zoneRepository = app('Modules\Iprice\Repositories\ZoneRepository');
+    return $zoneRepository->getItem(1, json_decode(json_encode(['filter' => ['field' => 'default']])));
+  }
+
+  public function getPriceAttribute()
+  {
+    $modelRelations = array_keys($this->getRelations());
+    $price = 0;
+    //Get price by default zone
+    if (in_array('prices', $modelRelations)) {
+      $defaultZone = $this->getDefaultZone();
+      if ($defaultZone) {
+        $defaultPrice = $this->prices->where('zone_id', $defaultZone->id)->first();
+        $price = $defaultPrice->price ?? 0;
+      }
+    }
+    //Response
+    return $price;
   }
 }
