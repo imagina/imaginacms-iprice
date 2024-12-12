837a0694729b29c960ad319d3b928acb83f87bb0
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-12-11 22:32:18 -0500

    Added: translations about cms in iprice

---
+++ Resources/lang/en/cms.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Prices",
+    "adminZones" => "Zones",
+  ],
+  "label" => [],
+  "form" => [
+  ],
+  "newZone" => "New Zone",
+  "updateZone" => "Update Zone"
+];
---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Precios",
+    "adminZones" => "Zonas",
+  ],
+  "label" => [],
+  "form" => [
+  ],
+  "newZone" => "Nueva Zona",
+  "updateZone" => "Actualizar Zona",
+];
